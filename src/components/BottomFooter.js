be2f8d2da7913ceef6abaccf740d8850a4c0c845
Ariba-memon <aribaabdulqadir786@gmail.com>
2021-05-16 10:57:56 -0700

    olxclone GIT

---
+++ src/components/BottomFooter.js
@@ -0,0 +1,31 @@
+import React from 'react'
+
+const BottomFooter = () => {
+    return (
+        <div>
+
+            <div className='bottom-footer'>
+
+                <p>Other Countries</p>
+                <h6>
+                    <a href='#'>India</a>-
+                    <a href='#'>South Africa</a>-
+                    <a href='#'>Indonesia</a>
+                </h6>
+
+                <p className='to-right'>Free Classifieds in Pakistan</p>
+                <h6 className='pad'>
+                    .
+                    © 2006-2021
+                    OLX
+                    Website by Ariba Abdul qadir
+                </h6>
+
+            </div>
+
+        </div>
+
+    )
+}
+
+export default BottomFooter
